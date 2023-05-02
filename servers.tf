dc29c7f3cb332acafe2a0d131808394ff25122cf
pavanbairu <pavan.bairu1@gmail.com>
2023-05-02 11:57:49 +0530

    creating instances using loops and for each in terraform script

--- servers.tf
+++ servers.tf
@@ -32,7 +32,7 @@ data "aws_security_group" "allow-all" {
 resource "aws_instance" "all_instance" {
   for_each = var.components
   ami = data.aws_ami.centos.image_id
-  instance_type = each.value["name"]
+  instance_type = each.value["instance_type"]
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
   tags = {
