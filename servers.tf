fe2d0780fb2785af0683d6ef30a58af140a7824a
pavanbairu <pavan.bairu1@gmail.com>
2023-05-02 12:04:58 +0530

    creating dns routing using loops and for each in terraform script

--- servers.tf
+++ servers.tf
@@ -40,3 +40,11 @@ resource "aws_instance" "all_instance" {
   }
 }
 
+resource "aws_route53_record" "records" {
+  for_each = var.components
+  name    = "${each.value["name"]}-dev.pavanbairu.tech"
+  type    = "A"
+  zone_id = "Z08846229MEF59DJAKAS"
+  ttl = 30
+  records = [aws_instance.all_instance[each.value["name"]].private_ip]
+}
