2a4208ce9a8e9cbf01fe9366c36a2ba098b4dca0
pavanbairu <pavan.bairu1@gmail.com>
2023-05-03 19:16:39 +0530

    created seperate terraform files for readability

---
+++ env-dev/main.tf
@@ -0,0 +1,16 @@
+components = {
+  frontend = {
+    name = "frontend"
+    instance_type = "t3.micro"
+  }
+  mongodb = {
+    name = "mongodb"
+    instance_type = "t3.micro"
+  }
+  catalogue  = {
+    name = "catalogue"
+    instance_type = "t3.micro"
+  }
+}
+
+env = "dev"
--- servers.tf
+++ servers.tf
@@ -1,34 +1,3 @@
-variable "components" {
-  default = {
-    frontend = {
-      name = "frontend"
-      instance_type = "t3.micro"
-    }
-    mongodb = {
-      name = "mongodb"
-      instance_type = "t3.micro"
-    }
-    catalogue  = {
-      name = "catalogue"
-      instance_type = "t3.micro"
-    }
-  }
-}
-
-data "aws_ami" "centos" {
-  owners      = ["973714476881"]
-  name_regex  = "Centos-8-DevOps-Practice"
-  most_recent = true
-}
-
-data "aws_security_group" "allow-all" {
-  name = "allow-all"
-}
-
-#variable "instance_type" {
-#  default = "t3.micro"
-#}
-
 resource "aws_instance" "all_instance" {
   for_each = var.components
   ami = data.aws_ami.centos.image_id
