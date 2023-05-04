35f58a9cfac3b6ec27ed78bc60180cc942933f48
pavanbairu <pavan.bairu1@gmail.com>
2023-05-04 11:07:18 +0530

    updated individual dynamic component ip address

--- servers.tf
+++ servers.tf
@@ -18,7 +18,7 @@ resource "null_resource" "provisioner" {
       type     = "ssh"
       user     = "centos"
       password = "DevOps321"
-      host     = aws_instance.all_instance.private_ip
+      host     = aws_instance.all_instance[each.value["name"]].private_ip
     }
 
     inline = [
