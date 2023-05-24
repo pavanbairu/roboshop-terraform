7b35776068722c23128cb7376f55d65508ec521f
pavanbairu <pavan.bairu1@gmail.com>
2023-05-24 23:13:59 +0530

    created the subnets

--- env-dev/main.tf
+++ env-dev/main.tf
@@ -3,5 +3,11 @@ env = "dev"
 vpc = {
   main = {
     cidr_block = "10.0.0.0/16"
+    subnets = {
+      public = {
+        cidr_block = ["10.0.0.0/24", "10.0.1.0/24"]
+        azs = ["us-east-1a", "us-east-1b"]
+      }
+    }
   }
 }
