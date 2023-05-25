73e63201478e3f87270137135e9b60df61b693a5
pavanbairu <pavan.bairu1@gmail.com>
2023-05-25 22:26:56 +0530

    fixing the subnet_id issue

--- main.tf
+++ main.tf
@@ -10,8 +10,8 @@ module "vpc" {
 
 module "web" {
   source = "git::https://github.com/pavanbairu/tf-module-app.git"
-  for_each = var.app
+
+  for_each      = var.app
   instance_type = each.value["instance_type"]
-  subnet_id = element(lookup(lookup(module.vpc, each.value["subnet_name"], null ), "subnet_ids", null ), 0)
-#  = element(lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null), 0)
+  subnet_id     = element(lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null), 0)
 }
