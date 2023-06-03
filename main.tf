9c6fd132263a8227cbe1857f48aa9b78cc0a531b
pavanbairu <pavan.bairu1@gmail.com>
2023-06-03 14:34:18 +0530

    creating docdb cluster instance

--- main.tf
+++ main.tf
@@ -48,5 +48,7 @@ module "docdb" {
   allow_db_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
   kms_arn       = var.kms_arn
   engine_version = each.value["engine_version"]
+  instance_count = each.value["instance_count"]
+  instance_class = each.value["instance_class"]
 }
 
