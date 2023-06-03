46f3c06159a83b3979da9c364b460d563c361eea
pavanbairu <pavan.bairu1@gmail.com>
2023-06-03 17:19:48 +0530

    creating the rds mysql db cluster and parameter group

--- env-dev/main.tf
+++ env-dev/main.tf
@@ -98,3 +98,33 @@ docdb = {
     instance_class = "db.t3.medium"
   }
 }
+
+rds = {
+  main = {
+    subnet_name    = "db"
+    allow_db_cidr  = "app"
+    engine_version = "5.7.mysql_aurora.2.11.2"
+    instance_count = 1
+    instance_class = "db.t3.small"
+  }
+}
+
+elasticache = {
+  main = {
+    subnet_name    = "db"
+    allow_db_cidr  = "app"
+    engine_version = "6.x"
+    replicas_per_node_group = 1
+    num_node_groups = 1
+    num_cache_nodes = 1
+    node_type = "cache.t3.micro"
+  }
+}
+
+rabbitmq = {
+  main = {
+    subnet_name    = "db"
+    allow_db_cidr  = "app"
+    instance_type = "t3.small"
+  }
+}
