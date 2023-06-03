d64db2d01862c99701199512b2a1c104dcc90641
pavanbairu <pavan.bairu1@gmail.com>
2023-06-03 12:30:07 +0530

    created docdb cluster and  parameter group

--- env-dev/main.tf
+++ env-dev/main.tf
@@ -1,8 +1,9 @@
-env = "dev"
-bastion_cidr = ["172.31.13.83/32"]
-default_vpc_id = "vpc-0a1bf336cf22dd9ab"
+env              = "dev"
+bastion_cidr     = ["172.31.13.83/32"]
+default_vpc_id   = "vpc-0a1bf336cf22dd9ab"
 default_vpc_cidr = "172.31.0.0/16"
 default_vpc_rtid = "rtb-097d2c148c9b47828"
+kms_arn          = "arn:aws:kms:us-east-1:416622536569:key/1dfa7b43-08ea-4a7f-b85d-4827823ac62e"
 
 vpc = {
   main = {
@@ -10,24 +11,24 @@ vpc = {
     subnets = {
 
       public = {
-        name = "public"
+        name       = "public"
         cidr_block = ["10.0.0.0/24", "10.0.1.0/24"]
-        azs = ["us-east-1a", "us-east-1b"]
+        azs        = ["us-east-1a", "us-east-1b"]
       }
       web = {
-        name = "web"
+        name       = "web"
         cidr_block = ["10.0.2.0/24", "10.0.3.0/24"]
-        azs = ["us-east-1a", "us-east-1b"]
+        azs        = ["us-east-1a", "us-east-1b"]
       }
       app = {
-        name = "app"
+        name       = "app"
         cidr_block = ["10.0.4.0/24", "10.0.5.0/24"]
-        azs = ["us-east-1a", "us-east-1b"]
+        azs        = ["us-east-1a", "us-east-1b"]
       }
       db = {
-        name = "db"
+        name       = "db"
         cidr_block = ["10.0.6.0/24", "10.0.7.0/24"]
-        azs = ["us-east-1a", "us-east-1b"]
+        azs        = ["us-east-1a", "us-east-1b"]
       }
     }
   }
@@ -35,54 +36,63 @@ vpc = {
 
 app = {
   frontend = {
-    name          = "frontend"
-    instance_type = "t3.small"
-    subnet_name   = "web"
-    allow_app_cidr = "public"
-    desired_capacity   = 2
-    max_size           = 10
-    min_size           = 2
+    name             = "frontend"
+    instance_type    = "t3.small"
+    subnet_name      = "web"
+    allow_app_cidr   = "public"
+    desired_capacity = 2
+    max_size         = 10
+    min_size         = 2
   }
   catalogue = {
-    name          = "catalogue"
-    instance_type = "t3.small"
-    subnet_name   = "app"
-    allow_app_cidr = "web"
-    desired_capacity   = 2
-    max_size           = 10
-    min_size           = 2
+    name             = "catalogue"
+    instance_type    = "t3.small"
+    subnet_name      = "app"
+    allow_app_cidr   = "web"
+    desired_capacity = 2
+    max_size         = 10
+    min_size         = 2
   }
-#  cart = {
-#    name          = "cart"
-#    instance_type = "t3.small"
-#    subnet_name   = "app"
-#    desired_capacity   = 2
-#    max_size           = 10
-#    min_size           = 2
-#  }
-#  user = {
-#    name          = "user"
-#    instance_type = "t3.small"
-#    subnet_name   = "app"
-#    desired_capacity   = 2
-#    max_size           = 10
-#    min_size           = 2
-#  }
-#  shipping = {
-#    name          = "shipping"
-#    instance_type = "t3.small"
-#    subnet_name   = "app"
-#    desired_capacity   = 2
-#    max_size           = 10
-#    min_size           = 2
-#  }
-#  payment = {
-#    name          = "payment"
-#    instance_type = "t3.small"
-#    subnet_name   = "app"
-#    desired_capacity   = 2
-#    max_size           = 10
-#    min_size           = 2
-#  }
+  #  cart = {
+  #    name          = "cart"
+  #    instance_type = "t3.small"
+  #    subnet_name   = "app"
+  #    desired_capacity   = 2
+  #    max_size           = 10
+  #    min_size           = 2
+  #  }
+  #  user = {
+  #    name          = "user"
+  #    instance_type = "t3.small"
+  #    subnet_name   = "app"
+  #    desired_capacity   = 2
+  #    max_size           = 10
+  #    min_size           = 2
+  #  }
+  #  shipping = {
+  #    name          = "shipping"
+  #    instance_type = "t3.small"
+  #    subnet_name   = "app"
+  #    desired_capacity   = 2
+  #    max_size           = 10
+  #    min_size           = 2
+  #  }
+  #  payment = {
+  #    name          = "payment"
+  #    instance_type = "t3.small"
+  #    subnet_name   = "app"
+  #    desired_capacity   = 2
+  #    max_size           = 10
+  #    min_size           = 2
+  #  }
 
+}
+
+
+docdb = {
+  main = {
+    subnet_name    = "db"
+    allow_db_cidr  = "app"
+    engine_version = "4.0.0"
+  }
 }
