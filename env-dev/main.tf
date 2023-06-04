e4bcd8179e8ac95c846c773cd6a2387a2c04fb22
pavanbairu <pavan.bairu1@gmail.com>
2023-06-04 11:35:29 +0530

    create load balancer target group and attached it under corresponding app autoscaling scaling group

--- env-dev/main.tf
+++ env-dev/main.tf
@@ -43,6 +43,7 @@ app = {
     desired_capacity = 1
     max_size         = 10
     min_size         = 1
+    app_port         = 80
   }
   catalogue = {
     name             = "catalogue"
@@ -52,6 +53,7 @@ app = {
     desired_capacity = 1
     max_size         = 10
     min_size         = 1
+    app_port         = 8080
   }
   #  cart = {
   #    name          = "cart"
--- main.tf
+++ main.tf
@@ -99,6 +99,7 @@ module "web" {
   desired_capacity  = each.value["desired_capacity"]
   max_size          = each.value["max_size"]
   min_size          = each.value["min_size"]
+  app_port          = each.value["app_port"]
 
   env           = var.env
   bastion_cidr  = var.bastion_cidr
